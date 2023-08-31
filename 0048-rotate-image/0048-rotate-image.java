6f2fbb9d1b46bc55396a2e4b374691efc9fa9746
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2023-08-31 20:21:51 +0530

    Time: 0 ms (100%), Space: 40.9 MB (96.92%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        // TC : O(N^2) + O(N * N/2) // SC : O(1)
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = i; j < matrix[0].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix.length/2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length-1-j];
+                matrix[i][matrix.length-1-j] = temp;
+            }
+        }
+    }
+}
