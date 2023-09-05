69e20691c92d92f0d1a6e7a660bce6932ec9063e
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2023-09-05 14:01:34 +0530

    Time: 6 ms (66.46%), Space: 53.1 MB (79.39%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,30 +1,30 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
         // TC : O(N) // SC : O(N)
-        Set<Integer> set = new HashSet<>();
-        for(int num : nums) {
-            set.add(num);
-        }
-        List<Integer> list = new ArrayList<>();
-        for(int i = 1; i <= nums.length; i++) {
-            if(!set.contains(i)) {
-                list.add(i);
+        // Set<Integer> set = new HashSet<>();
+        // for(int num : nums) {
+        //     set.add(num);
+        // }
+        // List<Integer> list = new ArrayList<>();
+        // for(int i = 1; i <= nums.length; i++) {
+        //     if(!set.contains(i)) {
+        //         list.add(i);
+        //     }
+        // }
+        // return list;
+        // TC : O(N) // SC : O(1)
+        List<Integer> ans = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++) {
+            int el = Math.abs(nums[i])-1;
+            if(nums[el] > 0) {
+                nums[el] = -nums[el];
             }
         }
-        return list;
-        // TC : O(N) // SC : O(1)
-        // List<Integer> ans = new ArrayList<>();
-        // for(int i = 0; i < nums.length; i++) {
-        //     int el = Math.abs(nums[i])-1;
-        //     if(nums[el] > 0) {
-        //         nums[el] = -nums[el];
-        //     }
-        // }
-        // for(int i = 0; i < nums.length; i++) {
-        //     if(nums[i] > 0) {
-        //         ans.add(i+1);
-        //     }
-        // }
-        // return ans;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] > 0) {
+                ans.add(i+1);
+            }
+        }
+        return ans;
     }
 }
