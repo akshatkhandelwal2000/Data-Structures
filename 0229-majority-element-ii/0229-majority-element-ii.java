7c76b4166d80dc65361eccbd874a5d4aee3e0381
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2023-10-05 23:55:30 +0530

    Time: 3 ms (68.13%), Space: 47.1 MB (10.72%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,16 +1,54 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
+        // Map<Integer, Integer> map = new HashMap<>();
+        // for(int i = 0; i < nums.length; i++) {
+        //     map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+        // }
+        // int n = nums.length;
+        // List<Integer> ans = new ArrayList<>();
+        // for(int i = 0; i < n; i++) {
+        //     if(map.get(nums[i]) > n/3 && !ans.contains(nums[i])) {
+        //         ans.add(nums[i]);
+        //     }
+        // }
+        // return ans;
+        int ele1 = -1;
+        int ele2 = -1;
+        int count1 = 0;;
+        int count2 = 0;
         for(int i = 0; i < nums.length; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
-        }
-        int n = nums.length;
-        List<Integer> ans = new ArrayList<>();
-        for(int i = 0; i < n; i++) {
-            if(map.get(nums[i]) > n/3 && !ans.contains(nums[i])) {
-                ans.add(nums[i]);
+            if(nums[i] != ele2 && count1 == 0) {
+                ele1 = nums[i];
+                count1++;
+            }
+            else if(nums[i] == ele1) {
+                count1++;
+            }
+            else if(nums[i] != ele1 && count2 == 0) {
+                ele2 = nums[i];
+                count2++;
+            }
+            else if(nums[i] == ele2) {
+                count2++;
+            }
+            else {
+                count1--;
+                count2--;
             }
         }
+        count1 = 0;
+        count2 = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == ele1) {
+                count1++;
+            }
+            else if(nums[i] == ele2) {
+                count2++;
+            }
+        }
+        List<Integer> ans = new ArrayList<>();
+        if(count1 > nums.length/3) ans.add(ele1);
+        if(count2 > nums.length/3) ans.add(ele2);
         return ans;
     }
 }
