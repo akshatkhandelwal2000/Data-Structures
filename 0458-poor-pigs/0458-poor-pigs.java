c2108ecf5b85dc6d279ea7cbe027278eff2d7cd7
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2023-10-30 02:04:46 +0530

    Time: 0 ms (100%), Space: 38.9 MB (76.64%) - LeetHub

---
+++ 0458-poor-pigs/0458-poor-pigs.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int t = minutesToTest/minutesToDie+1;
+        int pigs = 0;
+        while(Math.pow(t, pigs) < buckets) {
+            pigs++;
+        }
+        return pigs;
+    }
+}
