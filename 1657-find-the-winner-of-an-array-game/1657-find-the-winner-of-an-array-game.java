ac176481db522faa37d425d370ac55526c9793d1
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2023-11-06 01:59:37 +0530

    Time: 0 ms (100%), Space: 56.5 MB (65.94%) - LeetHub

--- 1657-find-the-winner-of-an-array-game/1657-find-the-winner-of-an-array-game.java
+++ 1657-find-the-winner-of-an-array-game/1657-find-the-winner-of-an-array-game.java
@@ -1,13 +1,13 @@
 class Solution {
     public int getWinner(int[] arr, int k) {
         // TC : O(N) // SC : O(1)
-        int max = 0;
-        if(k >= arr.length) {
-            for(int i = 0; i < arr.length; i++) {
-                max = Math.max(max, arr[i]);
-            }
-            return max; 
-        }
+        // int max = 0;
+        // if(k >= arr.length) {
+        //     for(int i = 0; i < arr.length; i++) {
+        //         max = Math.max(max, arr[i]);
+        //     }
+        //     return max; 
+        // }
         int winCount = 1;
         int maxKnown = Math.max(arr[0], arr[1]);
         for(int i = 2; i < arr.length; i++) {
