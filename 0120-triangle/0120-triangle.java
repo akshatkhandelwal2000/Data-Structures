169326713779b26e75ccdafc0d5c70186fe62d3b
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2023-12-31 00:10:08 +0530

    Time: 7 ms (23.08%), Space: 44.3 MB (30.86%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,5 +1,5 @@
-class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) {
+// class Solution {
+//     public int minimumTotal(List<List<Integer>> triangle) {
     // Memoization
     // Complexity Analysis
     // Time Complexity: O(N*N)
@@ -8,20 +8,45 @@ class Solution {
 
     // Space Complexity: O(N*N) + O(N)
     // Reason: We are using an external array of size ‘N*N’. The stack space will be O(N).
+    //     int n = triangle.size();
+    //     int[][] dp = new int[n][n];
+    //     for(int i = 0; i < n; i++) {
+    //         for(int j = 0; j < n; j++) {
+    //             dp[i][j] = -1;
+    //         }
+    //     }
+    //     return rec(0, 0, triangle, n, dp);
+    // }
+    // public int rec(int i, int j, List<List<Integer>> triangle, int n, int[][] dp) {
+    //     if(i == n-1) return triangle.get(n-1).get(j);
+    //     if(dp[i][j] != -1) return dp[i][j];
+    //     int down = triangle.get(i).get(j) + rec(i+1, j, triangle, n, dp);
+    //     int diagonal = triangle.get(i).get(j) + rec(i+1, j+1, triangle, n, dp);
+    //     return dp[i][j] = Math.min(down, diagonal);
+//     }
+// }
+
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+    // Tabulation
+    // Complexity Analysis
+    // Time Complexity: O(N*N)
+
+    // Reason: There are two nested loops
+
+    // Space Complexity: O(N*N)
+
+    // Reason: We are using an external array of size ‘N*N’. The stack space will be                eliminated.
         int n = triangle.size();
         int[][] dp = new int[n][n];
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                dp[i][j] = -1;
+        for(int j = 0; j < n; j++) dp[n-1][j] = triangle.get(n-1).get(j);
+        for(int i = n-2; i >= 0; i--) {
+            for(int j = i; j >= 0; j--) {
+                int down = triangle.get(i).get(j) + dp[i+1][j];
+                int diagonal = triangle.get(i).get(j) + dp[i+1][j+1];
+                dp[i][j] = Math.min(down, diagonal);
             }
         }
-        return rec(0, 0, triangle, n, dp);
-    }
-    public int rec(int i, int j, List<List<Integer>> triangle, int n, int[][] dp) {
-        if(i == n-1) return triangle.get(n-1).get(j);
-        if(dp[i][j] != -1) return dp[i][j];
-        int down = triangle.get(i).get(j) + rec(i+1, j, triangle, n, dp);
-        int diagonal = triangle.get(i).get(j) + rec(i+1, j+1, triangle, n, dp);
-        return dp[i][j] = Math.min(down, diagonal);
+        return dp[0][0];
     }
 }
