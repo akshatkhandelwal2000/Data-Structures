4f7673a98ab29e7728ffe459bd6d39d4c890c2cc
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-05-05 18:01:19 +0530

    Time: 1 ms (100%), Space: 41.9 MB (100%) - LeetHub

---
+++ 3136-valid-word/3136-valid-word.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String word) {
+        int n = word.length();
+        if(n < 3) return false;
+        int in = 0;
+        int nv = 0;
+        for(int i = 0; i < n; i++) {
+            char c = word.charAt(i);
+            if(!Character.isLetterOrDigit(c)) {
+                return false;
+            }
+            if(isVowel(c)) {
+                in++;
+            }
+            if(!isVowel(c) && Character.isLetter(c)) {
+                nv++;
+            }
+        }
+        if(in >= 1 && nv >= 1) return true;
+        return false;
+    }
+    public boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+    }
+}
