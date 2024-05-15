7c38dd7a60ee762431858aba721342550315e94e
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-05-16 00:57:36 +0530

    Time: 12 ms (58.31%), Space: 44.3 MB (70.98%) - LeetHub

---
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        // TC : O(N) // SC : O(N)
+        Stack<Character> st = new Stack<>();
+        for(char c : s.toCharArray()) {
+            st.push(c);
+            if(c == 'c') {
+                char ch = c;
+                int i = 0;
+                while(!st.isEmpty() && i < 3) {
+                    if(st.pop() != ch) break;
+                    ch--;
+                    i++;
+                }
+                if(i != 3) return false;
+            }
+        }
+        return st.isEmpty();
+    }
+}
