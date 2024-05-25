09ff91d0a72e544d74293ad2045f72a2f4dad31a
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-05-26 00:14:44 +0530

    Time: 33 ms (100%), Space: 61.4 MB (100%) - LeetHub

--- 3159-find-occurrences-of-an-element-in-an-array/3159-find-occurrences-of-an-element-in-an-array.java
+++ 3159-find-occurrences-of-an-element-in-an-array/3159-find-occurrences-of-an-element-in-an-array.java
@@ -1,5 +1,6 @@
 class Solution {
     public int[] occurrencesOfElement(int[] nums, int[] queries, int x) {
+        // TC : O(N + M) // SC : O(N)
         int n = nums.length;
         int m = queries.length;
         int[] ans = new int[m];
@@ -16,8 +17,7 @@ class Solution {
         }
         for(int i = 0; i < m; i++) {
             if(map.containsKey(x) && map.get(x).size() >= queries[i]) {
-                List<Integer> list2 = map.get(x);
-                ans[i] = list2.get(queries[i]-1);
+                ans[i] = map.get(x).get((queries[i]-1));
             }
             else {
                 ans[i] = -1;
