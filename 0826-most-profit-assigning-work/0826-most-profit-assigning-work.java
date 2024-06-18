fad6deecb587acfc0826750b4471ec7b711def04
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-06-18 23:47:26 +0530

    Time: 23 ms (45.7%), Space: 45.7 MB (49.14%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
-        // TC : O(NlogN) + O(NlogN) // SC : O(N) + O(N)
+        // TC : O(NlogN) // SC : O(N)
         int n = difficulty.length;
         int n2 = worker.length;
         int[][] job = new int[n][2];
@@ -9,19 +9,17 @@ class Solution {
             job[i][1] = profit[i];
         }
         Arrays.sort(job, (a,b) -> a[0]-b[0]);
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         int i = 0;
         int k = 0;
         Arrays.sort(worker);
         int ans = 0;
+        int max = 0;
         while(k < n2) {
             while(i < n && worker[k] >= job[i][0]) {
-                pq.offer(job[i][1]);
+                max = Math.max(max, job[i][1]);
                 i++;
             }
-            if(!pq.isEmpty()) {
-                ans += pq.peek();
-            }
+            ans += max;
             k++;
         }
         return ans;
