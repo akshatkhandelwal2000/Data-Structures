d2929f71c7dd329d7a950e6fedc671eb20b0662d
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-06-23 23:07:02 +0530

    Time: 71 ms (35.87%), Space: 56.3 MB (74.52%) - LeetHub

---
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int longestSubarray(int[] nums, int limit) {
+        // TC : O(NlogN) // SC : O(N)
+        int n = nums.length;
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        int i = 0;
+        int j = 0;
+        int ans = 0;
+        while(j < n) {
+            map.put(nums[j], map.getOrDefault(nums[j], 0)+1);
+            while(map.lastKey() - map.firstKey() > limit) {
+                map.put(nums[i], map.getOrDefault(nums[i], 0)-1);
+                if(map.get(nums[i]) == 0) {
+                    map.remove(nums[i]);
+                }
+                i++;
+            }
+            ans = Math.max(ans, j-i+1);
+            j++;
+        }
+        return ans;
+    }
+}
