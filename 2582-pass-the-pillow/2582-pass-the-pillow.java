25e27eec26f83a303668e289e42eb29d8bf1a869
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-07-06 14:44:40 +0530

    Time: 1 ms (28.53%), Space: 40.1 MB (61.02%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.java
+++ 2582-pass-the-pillow/2582-pass-the-pillow.java
@@ -1,19 +1,22 @@
 class Solution {
     public int passThePillow(int n, int time) {
-        // TC : O(N) // SC : O(1)
-        int temp = 0;
+        // TC : O(time) // SC : O(1)
         boolean flag = true;
         int i = 1;
-        while(temp != time) {
+        while(time != 0) {
             if(i == 1) {
                 flag = true;
             }
             if(i == n) {
                 flag = false;
             }
-            if(flag) i++;
-            if(!flag) i--;
-            temp++;
+            if(flag == true) {
+                i++;
+            }
+            else if(flag == false){
+                i--;
+            }
+            time--;
         }
         return i;
     }
