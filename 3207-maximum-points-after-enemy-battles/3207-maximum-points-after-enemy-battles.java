b55d9fb65c4c34be02b5908cbc88c4ed0fb0ccd3
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-07-06 21:39:15 +0530

    Time: 23 ms (100%), Space: 56.6 MB (100%) - LeetHub

---
+++ 3207-maximum-points-after-enemy-battles/3207-maximum-points-after-enemy-battles.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long maximumPoints(int[] enemyEnergies, int currentEnergy) {
+        int n = enemyEnergies.length;
+        Arrays.sort(enemyEnergies);
+        if(currentEnergy < enemyEnergies[0]) {
+            return 0;
+        }
+        long sum = 0;
+        for(int i = 1; i < n; i++) {
+          sum += enemyEnergies[i];
+        }
+        return (sum+currentEnergy)/enemyEnergies[0];
+    }
+}
