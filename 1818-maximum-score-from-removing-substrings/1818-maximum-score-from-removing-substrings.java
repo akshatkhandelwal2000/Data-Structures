98b7347b72372de9d664189ec5c4273d3c968b0b
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-07-13 01:27:49 +0530

    Time: 173 ms (55.24%), Space: 46.4 MB (46.67%) - LeetHub

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.java
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.java
@@ -2,26 +2,9 @@ class Solution {
     public int maximumGain(String s, int x, int y) {
         // TC : O(N) + O(N) // SC : O(N)
         int n = s.length();
-        String str = "";
-        String ss = "";
-        int max = 0;
-        int min = 0;
-        if(x <= y) {
-            ss = "ab";
-            min = x;
-        }
-        else if(y < x) {
-            ss = "ba";
-            min = y;
-        }
-        if(x >= y) {
-            str = "ab"; 
-            max = x;
-        }
-        else {
-            str = "ba";
-            max = y;
-        }
+        int max = Math.max(x, y);
+        int min = Math.min(x, y);
+        String str = x >= y ? "ab" : "ba";
         Stack<Character> st = new Stack<>();
         int i = 0;
         int ans = 0;
@@ -40,10 +23,8 @@ class Solution {
         sb.reverse();
         String sbs = sb.toString();
         i = 0;
-        st.clear();
-        st = new Stack<>();
         while(i < sbs.length()) {
-            if(!st.isEmpty() && sbs.charAt(i) == ss.charAt(1) && st.peek() == ss.charAt(0)) {
+            if(!st.isEmpty() && sbs.charAt(i) == str.charAt(0) && st.peek() == str.charAt(1)) {
                 ans += min;
                 st.pop();
             }
