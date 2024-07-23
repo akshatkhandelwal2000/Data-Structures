c6fe7e201ce07e3308f299a7dc439aa9f22bf614
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-07-23 18:13:22 +0530

    Time: 5 ms (92.24%), Space: 44.5 MB (25.8%) - LeetHub

--- 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.java
+++ 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.java
@@ -1,39 +1,40 @@
-class Pair{
-    int val;
-    int freq;
-    public Pair(int val, int freq) {
-        this.val = val;
-        this.freq = freq;
-    }
-}
 class Solution {
+    class Pair {
+        int val;
+        int freq;
+        public Pair(int val, int freq) {
+            this.val = val;
+            this.freq = freq;
+        }
+    }
     public int[] frequencySort(int[] nums) {
-        // TC : O(NlogN) // SC : O(N + N)
+        // TC : O(NlogN) // SC : O(N + N) 
+        int n = nums.length;
+        int[] ans = new int[n];
         Map<Integer, Integer> map = new HashMap<>();
-        for(int val : nums) {
-            map.put(val, map.getOrDefault(val, 0)+1);
+        for(int i = 0; i < n; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
         }
         PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> {
             if(a.freq == b.freq) {
-                return b.val - a.val;
+                return b.val-a.val;
             }
             else {
-                return a.freq - b.freq;
+                return a.freq-b.freq;
             }
         });
-        for(Map.Entry<Integer, Integer> e : map.entrySet()) {
-            pq.add(new Pair(e.getKey(), e.getValue()));
+        for(int i : map.keySet()) {
+            pq.offer(new Pair(i, map.get(i)));
         }
-        int[] ans = new int[nums.length];
-        int k = 0;
+        int index = 0;
         while(!pq.isEmpty()) {
-            int va = pq.peek().val;
-            int fre = pq.peek().freq;
-            for(int i = 0; i < fre; i++) {
-                ans[k++] = va;
-            }
+            int val = pq.peek().val;
+            int freq = pq.peek().freq;
             pq.poll();
+            for(int i = 0; i < freq; i++) {
+                ans[index++] = val;
+            }
         }
-        return ans; 
+        return ans;
     }
 }
