0330708e605c6b7bb30e24fff910b5a6f385f6e7
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-07-24 18:37:51 +0530

    Time: 386 ms (34%), Space: 45 MB (17.15%) - LeetHub

---
+++ 2594-count-pairs-of-similar-strings/2594-count-pairs-of-similar-strings.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int similarPairs(String[] words) {
+        int n = words.length;
+        int ans = 0;
+        for(int i = 0; i < n; i++) {
+            Map<Character, Integer> map = new HashMap<>();
+            String word = words[i];
+            for(int j = 0; j < word.length(); j++) {
+                char c = word.charAt(j); 
+                map.put(c, map.getOrDefault(c, 0)+1);
+            }
+            for(int k = i+1; k < n; k++) {
+                boolean flag = true;
+                Map<Character, Integer> map2 = new HashMap<>();
+                String s = words[k];
+                for(int l = 0; l < s.length(); l++) {
+                    char c = s.charAt(l); 
+                    map2.put(c, map2.getOrDefault(c, 0)+1);
+                }
+                if(map.size() != map2.size()) {
+                    flag = false;
+                }
+                for(char v : map.keySet()) {
+                    if(!map2.containsKey(v)) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag) ans++;
+            }
+        } 
+        return ans;
+    }
+}
