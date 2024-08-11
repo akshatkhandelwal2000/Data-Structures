465992780862398a9e3169544023ab31e1c360a4
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-08-11 19:13:33 +0530

    Time: 2 ms (64.12%), Space: 59.3 MB (35.12%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -1,22 +1,18 @@
 class Solution {
     public boolean isMonotonic(int[] nums) {
+        // TC : O(N) // SC : O(1)
         int n = nums.length;
         boolean flag = true;
+        boolean flag2 = true;
         for(int i = 0; i < n-1; i++) {
             if(nums[i] > nums[i+1]) {
                 flag = false;
-                break;
             }
-        }
-        if(flag == true) return true;
-        boolean flag2 = true;
-        for(int i = 0; i < n-1; i++) {
             if(nums[i] < nums[i+1]) {
                 flag2 = false;
-                break;
             }
         }
-        if(flag2 == true) return true;
+        if(flag == true || flag2 == true) return true;
         return false;
     }
 }
