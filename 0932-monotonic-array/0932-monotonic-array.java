caa95a2d6fafe6489fcb28513385f0d2021ee44d
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-08-11 19:11:12 +0530

    Time: 1 ms (98.47%), Space: 56.8 MB (97.54%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.java
+++ 0932-monotonic-array/0932-monotonic-array.java
@@ -1,18 +1,22 @@
 class Solution {
     public boolean isMonotonic(int[] nums) {
-        // TC : O(N) // SC : O(1)
         int n = nums.length;
         boolean flag = true;
-        boolean flag2 = true;
         for(int i = 0; i < n-1; i++) {
             if(nums[i] > nums[i+1]) {
                 flag = false;
-            }
-            if(nums[i] < nums[i+1]) {
-                flag2 = false;
+                break;
             }
         }
-        if(flag == true || flag2 == true) return true;
+        if(flag == true) return true;
+        boolean flag2 = true;
+        for(int i = 0; i < n-1; i++) {
+            if(nums[i] < nums[i+1]) {
+                flag2 = false;
+                break;
+            }
+        }
+        if(flag2 == true) return true;
         return false;
     }
 }
