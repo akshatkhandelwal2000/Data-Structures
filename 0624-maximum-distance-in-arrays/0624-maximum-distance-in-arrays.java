165fe75da63c5ed4ebeeb518dc89fd546da3fc1b
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-08-17 01:16:58 +0530

    Time: 12 ms (28.75%), Space: 64 MB (92.92%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.java
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.java
@@ -5,29 +5,24 @@ class Solution {
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
         int in = -1;
+        int min2 = Integer.MAX_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int in2 = -1;
         for(int i = 0; i < n; i++) {
             if(arrays.get(i).get(arrays.get(i).size()-1) > max) {
                 max = arrays.get(i).get(arrays.get(i).size()-1);
                 in = i;
             }
-        }
-        for(int i = 0; i < n; i++) {
-            if(i == in) continue;
-            if(arrays.get(i).get(0) < min) {
-                min = arrays.get(i).get(0);
-            }
-        }
-        int min2 = Integer.MAX_VALUE;
-        int max2 = Integer.MIN_VALUE;
-        int in2 = -1;
-        for(int i = 0; i < n; i++) {
             if(arrays.get(i).get(0) < min2) {
                 min2 = arrays.get(i).get(0);
                 in2 = i;
             }
         }
         for(int i = 0; i < n; i++) {
-            if(i == in2) continue;
+            if(i == in && i == in2) continue;
+            if(arrays.get(i).get(0) < min) {
+                min = arrays.get(i).get(0);
+            }
             if(arrays.get(i).get(arrays.get(i).size()-1) > max2) {
                 max2 = arrays.get(i).get(arrays.get(i).size()-1);
             }
