a0463782fcb9d540c7b79486cc0907ec2710224f
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-08-17 01:16:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.java
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.java
@@ -5,30 +5,22 @@ class Solution {
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
         int in = -1;
-        int min2 = Integer.MAX_VALUE;
-        int max2 = Integer.MIN_VALUE;
-        int in2 = -1;
         for(int i = 0; i < n; i++) {
-            if(arrays.get(i).get(arrays.get(i).size()-1) > max) {
-                max = arrays.get(i).get(arrays.get(i).size()-1);
-                in = i;
-            }
-            if(arrays.get(i).get(0) < min2) {
-                min2 = arrays.get(i).get(0);
-                in2 = i;
+            for(int j = 0; j < arrays.get(i).size(); j++) {
+                if(arrays.get(i).get(j) > max) {
+                    max = arrays.get(i).get(j);
+                    in = i;
+                }
             }
         }
         for(int i = 0; i < n; i++) {
-            if(i == in && i == in2) continue;
-            if(arrays.get(i).get(0) < min) {
-                min = arrays.get(i).get(0);
-            }
-            if(arrays.get(i).get(arrays.get(i).size()-1) > max2) {
-                max2 = arrays.get(i).get(arrays.get(i).size()-1);
+            if(i == in) continue;
+            for(int j = 0; j < arrays.get(i).size(); j++) {
+                if(arrays.get(i).get(j) < min) {
+                    min = arrays.get(i).get(j);
+                }
             }
         }
-        int ans = Math.abs(max-min);
-        int ans2 = Math.abs(max2-min2); 
-        return Math.max(ans, ans2);
+        return Math.abs(max-min);
     }
 }
