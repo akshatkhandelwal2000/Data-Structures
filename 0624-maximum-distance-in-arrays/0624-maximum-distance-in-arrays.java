de44f386dae8bfef43f567710fd4774a37005d25
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-08-17 01:10:17 +0530

    Time: 8 ms (72.5%), Space: 65 MB (18.33%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.java
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.java
@@ -6,38 +6,30 @@ class Solution {
         int max = Integer.MIN_VALUE;
         int in = -1;
         for(int i = 0; i < n; i++) {
-            for(int j = 0; j < arrays.get(i).size(); j++) {
-                if(arrays.get(i).get(j) > max) {
-                    max = arrays.get(i).get(j);
-                    in = i;
-                }
+            if(arrays.get(i).get(arrays.get(i).size()-1) > max) {
+                max = arrays.get(i).get(arrays.get(i).size()-1);
+                in = i;
             }
         }
         for(int i = 0; i < n; i++) {
             if(i == in) continue;
-            for(int j = 0; j < arrays.get(i).size(); j++) {
-                if(arrays.get(i).get(j) < min) {
-                    min = arrays.get(i).get(j);
-                }
+            if(arrays.get(i).get(0) < min) {
+                min = arrays.get(i).get(0);
             }
         }
         int min2 = Integer.MAX_VALUE;
         int max2 = Integer.MIN_VALUE;
         int in2 = -1;
         for(int i = 0; i < n; i++) {
-            for(int j = 0; j < arrays.get(i).size(); j++) {
-                if(arrays.get(i).get(j) < min2) {
-                    min2 = arrays.get(i).get(j);
-                    in2 = i;
-                }
+            if(arrays.get(i).get(0) < min2) {
+                min2 = arrays.get(i).get(0);
+                in2 = i;
             }
         }
         for(int i = 0; i < n; i++) {
             if(i == in2) continue;
-            for(int j = 0; j < arrays.get(i).size(); j++) {
-                if(arrays.get(i).get(j) > max2) {
-                    max2 = arrays.get(i).get(j);
-                }
+            if(arrays.get(i).get(arrays.get(i).size()-1) > max2) {
+                max2 = arrays.get(i).get(arrays.get(i).size()-1);
             }
         }
         int ans = Math.abs(max-min);
