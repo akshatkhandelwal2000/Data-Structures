38375d8f334e92bebbcefbec1375f5ed56cf0d7a
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-08-18 14:24:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3259-maximum-energy-boost-from-two-drinks/3259-maximum-energy-boost-from-two-drinks.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long maxEnergyBoost(int[] energyDrinkA, int[] energyDrinkB) {
+        int n = energyDrinkA.length;
+        long[][] dp = new long[n][n];
+        dp[0][0] = energyDrinkA[0];
+        dp[0][1] = energyDrinkB[0];
+        for(int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i-1][0]+energyDrinkA[i], dp[i-1][1]);
+            dp[i][1] = Math.max(dp[i-1][1]+energyDrinkB[i], dp[i-1][0]);
+        }
+        return Math.max(dp[n-1][0], dp[n-1][1]);
+    }
+}
