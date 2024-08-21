41733142eb41a47fe6dfa8af6d3991096b439602
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-08-22 02:11:18 +0530

    Time: 18 ms (74.85%), Space: 55.4 MB (95.03%) - LeetHub

---
+++ 1945-finding-the-users-active-minutes/1945-finding-the-users-active-minutes.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] findingUsersActiveMinutes(int[][] logs, int k) {
+        int n = logs.length;
+        int[] ans = new int[k];
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        for(int i = 0; i < n; i++) {
+            int id = logs[i][0];
+            int time = logs[i][1];
+            if(!map.containsKey(id)) {
+                map.put(id, new HashSet<>());
+                map.get(id).add(time);
+            }
+            else map.get(id).add(time);
+        }
+        for(int i : map.keySet()) {
+            int in = map.get(i).size();
+            ans[in-1]++;
+        }
+        return ans;
+    }
+}
