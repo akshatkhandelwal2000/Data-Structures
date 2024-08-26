ec0eb0900074f8a75e9c9fbad1ec464ee7626225
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-08-26 17:38:52 +0530

    Time: 0 ms (100%), Space: 44.7 MB (48.03%) - LeetHub

--- 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.java
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.java
@@ -19,32 +19,32 @@ class Node {
 
 class Solution {
     public List<Integer> preorder(Node root) {
-    //     // TC : O(N) // SC : O(N)
-    //     List<Integer> ans = new ArrayList<>();
-    //     if(root == null) return ans;
-    //     rec(root, ans);
-    //     return ans;
-    // }
-    // public void rec(Node root, List<Integer> ans) {
-    //     if(root == null) return;
-    //     ans.add(root.val);
-    //     for(int i = 0; i < root.children.size(); i++) {
-    //         rec(root.children.get(i), ans);
-    //     }
-    
-        // Iterative
         // TC : O(N) // SC : O(N)
         List<Integer> ans = new ArrayList<>();
         if(root == null) return ans;
-        Stack<Node> st = new Stack<>();
-        st.push(root);
-        while(!st.isEmpty()) {
-            Node currentNode = st.pop();
-            ans.add(currentNode.val);
-            for(int i = currentNode.children.size()-1; i >= 0; i--) {
-                st.push(currentNode.children.get(i));
-            }
-        }
+        rec(root, ans);
         return ans;
+    }
+    public void rec(Node root, List<Integer> ans) {
+        if(root == null) return;
+        ans.add(root.val);
+        for(int i = 0; i < root.children.size(); i++) {
+            rec(root.children.get(i), ans);
+        }
+
+        // Iterative
+        // TC : O(N) // SC : O(N)
+        // List<Integer> ans = new ArrayList<>();
+        // if(root == null) return ans;
+        // Stack<Node> st = new Stack<>();
+        // st.push(root);
+        // while(!st.isEmpty()) {
+        //     Node currentNode = st.pop();
+        //     ans.add(currentNode.val);
+        //     for(int i = currentNode.children.size()-1; i >= 0; i--) {
+        //         st.push(currentNode.children.get(i));
+        //     }
+        // }
+        // return ans;
     }
 }
