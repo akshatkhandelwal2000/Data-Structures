6acff1a81449ff38d187c22ed5b4692d3665e293
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-08-26 17:20:42 +0530

    Time: 3 ms (24.14%), Space: 44.8 MB (34.61%) - LeetHub

---
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.java
@@ -0,0 +1,51 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<Integer> postorder(Node root) {
+        // TC : O(N) // SC : O(N)
+    //     List<Integer> ans = new ArrayList<>();
+    //     if(root == null) return ans;
+    //     rec(root, ans);
+    //     return ans;
+    // }
+    // public void rec(Node root, List<Integer> ans) {
+    //     if(root == null) return;
+    //     for(int i = 0; i < root.children.size(); i++) {
+    //         rec(root.children.get(i), ans);
+    //     }
+    //     ans.add(root.val);
+
+        // Iterative - do the reverse post order traversal
+        // TC : O(N) // SC : O(N)
+        List<Integer> ans = new ArrayList<>();
+        if(root == null) return ans;
+        Stack<Node> st = new Stack<>();
+        st.push(root);
+        while(!st.isEmpty()) {
+            Node currentNode = st.pop();
+            ans.add(currentNode.val);
+            for(int i = 0; i < currentNode.children.size(); i++) {
+                st.push(currentNode.children.get(i));
+            }
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+}
