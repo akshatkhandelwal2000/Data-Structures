d53541649f7b8f11a852a38ecfbf6d94a2fe6e47
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-09-03 20:50:09 +0530

    Time: 120 ms (29.9%), Space: 142.8 MB (61.01%) - LeetHub

--- 3275-k-th-nearest-obstacle-queries/3275-k-th-nearest-obstacle-queries.java
+++ 3275-k-th-nearest-obstacle-queries/3275-k-th-nearest-obstacle-queries.java
@@ -1,17 +1,17 @@
 class Solution {
     public int[] resultsArray(int[][] queries, int k) {
+        // TC : O(NlogK) // SC : O(N)
         int n = queries.length;
         int[] ans = new int[n];
-        PriorityQueue<Long> pq = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         for(int i = 0; i < n; i++) {
-            pq.offer((long)(Math.abs(queries[i][0])+Math.abs(queries[i][1])));
+            pq.offer((Math.abs(queries[i][0])+Math.abs(queries[i][1])));
             if(pq.size() < k) {
                 ans[i] = -1;
                 continue;
             }
             if(pq.size() > k) pq.poll();
-            long v = pq.peek();
-            ans[i] = (int)v;
+            ans[i] = pq.peek();
         }
         return ans;
     }
