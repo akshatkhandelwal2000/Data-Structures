eab4ebc8834dc7d6882727244a5cbd0e4b8c0e65
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-09-26 23:17:14 +0530

    Time: 0 ms (100%), Space: 41.4 MB (19.85%) - LeetHub

--- 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.java
+++ 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.java
@@ -1,8 +1,7 @@
 class Solution {
     public boolean haveConflict(String[] event1, String[] event2) {
         // TC : O(1) // SC : O(1)
-        if(event2[0].charAt(0) > event1[1].charAt(0) &&
-        event2[0].charAt(1) > event1[1].charAt(1)) return false;
-        return true;
+        return event2[0].compareTo(event1[1]) <= 0 &&
+        event1[0].compareTo(event2[1]) <= 0;
     }
 }
