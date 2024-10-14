f926754fc44cabdfe9f0c3292b2f6b1f7160b301
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-10-14 23:07:40 +0530

    Time: 159 ms (25.12%), Space: 63.1 MB (37.91%) - LeetHub

--- 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.java
+++ 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.java
@@ -1,16 +1,16 @@
 class Solution {
     public long maxKelements(int[] nums, int k) {
         // TC : O(NlogN) // SC : O(N)
+        int n = nums.length;
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         for(int i : nums) {
             pq.offer(i);
         }
         long ans = 0;
-        while(k > 0) {
-            double i = Math.ceil((double)pq.peek()/3);
-            ans += pq.poll();
-            pq.offer((int)i);
-            k--;
+        while(k-- > 0 && !pq.isEmpty()) {
+            ans += pq.peek();
+            double num = Math.ceil(pq.poll()/3.0);
+            pq.offer((int)(num));
         }
         return ans;
     }
