5caa7c9938868318ffcf29ee7200eb6059bc4609
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-10-30 01:01:05 +0530

    Time: 6 ms (80.77%), Space: 56.4 MB (51.45%) - LeetHub

--- 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.java
+++ 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.java
@@ -1,6 +1,6 @@
 class Solution {
-    int[] dir = {-1,0,1};
     public int maxMoves(int[][] grid) {
+        // TC : O(M*N) // SC : O(M*N)
         int m = grid.length;
         int n = grid[0].length;
         int[][] dp = new int[m][n];
@@ -8,19 +8,20 @@ class Solution {
             Arrays.fill(row, -1);
         }
         int ans = 0;
+        int[] dir = {-1,0,1};
         for(int i = 0; i < m; i++) {
-            ans = Math.max(ans, moves(i, 0, grid, dp));
+            ans = Math.max(ans, moves(i, 0, grid, dp, dir));
         }
         return ans;
     }
-    public int moves(int i, int j, int[][] grid, int[][] dp) {
+    public int moves(int i, int j, int[][] grid, int[][] dp, int[] dir) {
         if(dp[i][j] != -1) return dp[i][j];
         int moves = 0;
         for(int k = 0; k < 3; k++) {
             int a = i+dir[k];
             int b = j+1;
             if(a >= 0 && b >= 0 && a < grid.length && b < grid[0].length && grid[i][j] < grid[a][b]) {
-            moves = Math.max(moves, 1+moves(a, b, grid, dp));
+            moves = Math.max(moves, 1+moves(a, b, grid, dp, dir));
             }
         }
         dp[i][j] = moves;
