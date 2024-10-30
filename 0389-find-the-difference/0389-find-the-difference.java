0ee7314dc735b98e3e4802524ee4bb0599f682d6
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-10-30 21:32:30 +0530

    Time: 11 ms (30.92%), Space: 41.6 MB (76.37%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,12 +1,13 @@
 class Solution {
     public char findTheDifference(String s, String t) {
+        // TC : O(N + 26) // SC : O(26)
         int n = t.length();
         Map<Character, Integer> map = new HashMap<>();
         for(int i = 0; i < n; i++) {
             map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0)+1);
-        }
-        for(int i = 0; i < s.length(); i++) {
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0)-1);
+            if(i < n-1) {
+                map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0)-1);
+            }
         }
         for(char c : map.keySet()) {
             if(map.get(c) == 1) {
