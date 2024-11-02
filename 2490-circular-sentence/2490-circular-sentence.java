f0cea15d4e978fdcb6ec66b2e5f1c48e9c3f794c
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-11-02 14:08:20 +0530

    Time: 1 ms (96.81%), Space: 41.8 MB (83.14%) - LeetHub

---
+++ 2490-circular-sentence/2490-circular-sentence.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isCircularSentence(String sentence) {
+        if(sentence.charAt(0) != sentence.charAt(sentence.length()-1)) {
+            return false;
+        }
+        String[] words = sentence.split(" ");
+        int n = words.length;
+        String s = words[0];
+        for(int i = 1; i < n; i++) {
+            String word = words[i];
+            if(s.charAt(s.length()-1) != word.charAt(0)) {
+                return false;
+            }
+            s = word;
+        }
+        return true;
+    }
+}
