3c3a428614334014ad0a504cbb57d7f4af94ead5
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-11-03 16:17:30 +0530

    Time: 0 ms (100%), Space: 41.6 MB (27.67%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length() != goal.length()) {
+            return false;
+        }
+        int n = s.length();
+        StringBuilder sb = new StringBuilder(s);
+        for(int i = 0; i < n; i++) {
+            if(sb.substring(i, sb.length()).equals(goal)) {
+                return true;
+            }
+            sb.append(sb.charAt(i));
+        }
+        return false;
+    }
+}
