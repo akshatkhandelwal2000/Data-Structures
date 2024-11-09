825cb435b69c87076a6b1cf3d7445b6f80639bf3
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-11-10 00:31:40 +0530

    Time: 14 ms (44.13%), Space: 45.2 MB (74.22%) - LeetHub

---
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] findXSum(int[] nums, int k, int x) {
+        int  n = nums.length;
+        int[] ans = new int[n-k+1];
+        for(int i = 0; i < n-k+1; i++) {
+            Map<Integer, Integer> map = new HashMap<>();
+            PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> {
+                if(a[1] == b[1]) {
+                    return b[0]-a[0];
+                }
+                else {
+                    return b[1]-a[1];
+                }
+            });
+            for(int j = i; j <= i+k-1; j++) {
+                map.put(nums[j], map.getOrDefault(nums[j], 0)+1);
+            }
+            for(int a : map.keySet()) {
+                pq.offer(new int[]{a, map.get(a)});
+            }
+            int b = x;
+            int count = 0;
+            while(b-- > 0 && !pq.isEmpty()) {
+                for(int c = 0; c < pq.peek()[1]; c++) {
+                    count += pq.peek()[0];
+                }
+                pq.poll();
+            }
+            ans[i] = count;
+        }
+        return ans;
+    }
+}
