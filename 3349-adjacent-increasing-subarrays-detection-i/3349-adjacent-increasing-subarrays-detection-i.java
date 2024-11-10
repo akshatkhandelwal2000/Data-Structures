cf75c07ccdcd57ed97dfb279f097e24c0a00b9ad
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-11-11 00:12:03 +0530

    Time: 1 ms (100%), Space: 44.3 MB (100%) - LeetHub

---
+++ 3349-adjacent-increasing-subarrays-detection-i/3349-adjacent-increasing-subarrays-detection-i.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
+        if(k == 1) {
+            return true;
+        }
+        int n = nums.size();
+        int ans = 0;
+        int count = 1;
+        int a = 0;
+        for(int i = 1; i < n; i++) {
+            if(nums.get(i) > nums.get(i-1)) {
+                count++;
+            }
+            else {
+                a = count;
+                count = 1;
+            }
+            ans = Math.max(ans, Math.max(count/2, Math.min(a, count)));
+        }
+        return ans >= k;
+    }
+}
