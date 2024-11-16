fa55f3505c3fe7270c78a899bf91ddb0b9c4712f
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-11-16 22:31:20 +0530

    Time: 3 ms (25.19%), Space: 45.6 MB (13.03%) - LeetHub

--- 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.java
+++ 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.java
@@ -1,19 +1,22 @@
 class Solution {
     public int[] resultsArray(int[] nums, int k) {
-        // TC : O(N*K) // SC : O(N-K+1)
         int n = nums.length;
-        if(k == 1) return nums;
+        if(k == 1) {
+            return nums;
+        }
         int[] ans = new int[n-k+1];
         for(int i = 0; i < n-k+1; i++) {
-            int m = -1;
+            int max = 0;
             for(int j = i+1; j <= i+k-1; j++) {
-                m = Math.max(m, nums[j]);
-                if(nums[j] != nums[j-1]+1) {
-                    m = -1;
+                if(nums[j] < nums[j-1] || nums[j]-1 != nums[j-1]) {
+                    ans[i] = -1;
                     break;
                 }
+                max = Math.max(max, nums[j]);
             }
-            ans[i] = m;
+            if(ans[i] != -1) {
+                ans[i] = max;
+            }
         }
         return ans;
     }
