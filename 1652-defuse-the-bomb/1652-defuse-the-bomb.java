cfc85220f68ac224f72651e15aed5acd6860a6dc
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-11-19 00:28:14 +0530

    Time: 2 ms (67.36%), Space: 42.1 MB (55.38%) - LeetHub

---
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int n = code.length;
+        int[] ans = new int[n];
+        for(int i = 0; i < n; i++) {
+            if(k > 0) {
+                int abs = k; 
+                int j = i+1;
+                int sum = 0;
+                while(abs-- > 0) {
+                    sum += code[j%n];
+                    j++;
+                }
+                ans[i] = sum;
+            }
+            else if(k < 0) {
+                int abs = Math.abs(k);
+                int j = i-1;
+                int sum = 0;
+                while(abs-- > 0) {
+                    if(j < 0) {
+                        j = n-1;
+                    }
+                    sum += code[Math.abs(j)%n];
+                    j--;
+                }
+                ans[i] = sum;
+            }
+        }
+        return ans;
+    }
+}
