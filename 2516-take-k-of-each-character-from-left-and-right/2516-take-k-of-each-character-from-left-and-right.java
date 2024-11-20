57c9fb81a6aef1bae0409d85dbb8dbfa199aab7b
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-11-21 00:54:58 +0530

    Time: 15 ms (49.09%), Space: 45 MB (83.43%) - LeetHub

---
+++ 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int takeCharacters(String s, int k) {
+        int n = s.length();
+        int[] map = new int[3];
+        for(int i = 0; i < n; i++) {
+            map[s.charAt(i)-'a']++;
+        }
+        for(int i = 0; i < 3; i++) {
+            if(map[i] < k) {
+                return -1;
+            }
+        }
+        int ans = 0;
+        int i = 0;
+        int j = 0;
+        int[] count = new int[3];
+        while(j < n) {
+            count[s.charAt(j)-'a']++;
+            while(i <= j && map[0] - count[0] < k || map[1] - count[1] < k || map[2] - count[2] < k) {
+                count[s.charAt(i)-'a']--;
+                i++;
+            }
+            ans = Math.max(ans, j-i+1);
+            j++;
+        }
+        return n-ans;
+    }
+}
