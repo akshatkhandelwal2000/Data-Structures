4873f99e7556150752cd1322880ae4a0ac3c566c
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-11-27 01:21:51 +0530

    Time: 1 ms (100%), Space: 45.8 MB (45.99%) - LeetHub

--- 2924-find-champion-ii/2924-find-champion-ii.java
+++ 2924-find-champion-ii/2924-find-champion-ii.java
@@ -2,28 +2,19 @@ class Solution {
     public int findChampion(int n, int[][] edges) {
         // TC : O(N) // SC : O(N)
         int m = edges.length;
-        if(m == 0 && n == 1) {
-            return 0;
-        }
-        Set<Integer> set = new HashSet<>();
-        int max = 0;
-        Set<Integer> b = new HashSet<>();
+        int[] a = new int[n];
         for(int i = 0; i < m; i++) {
-            set.add(edges[i][1]);
-            b.add(edges[i][1]);
+            a[edges[i][1]]++;
         }
         int ans = 0;
         int count = 0;
-        Set<Integer> a = new HashSet<>();
-        for(int i = 0; i < m; i++) {
-            if(!set.contains(edges[i][0]) && !a.contains(edges[i][0])) {
-                ans = edges[i][0];
-                a.add(edges[i][0]);
+        for(int i = 0; i < n; i++) {
+            if(a[i] == 0) {
                 count++;
+                ans = i;
             }
-            b.add(edges[i][0]);
         }
-        if(count > 1 || b.size() != n) {
+        if(count > 1) {
             return -1;
         }
         return ans;
