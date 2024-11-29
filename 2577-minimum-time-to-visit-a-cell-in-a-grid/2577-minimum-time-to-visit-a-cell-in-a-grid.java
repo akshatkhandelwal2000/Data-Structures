0646da3bf25a4746c75ce702e4bcc4b5165219c8
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-11-30 00:23:40 +0530

    Time: 349 ms (5.15%), Space: 58.5 MB (60.2%) - LeetHub

--- 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.java
+++ 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.java
@@ -1,48 +1,44 @@
 class Solution {
-    class Triplet {
-        int time;
-        int row;
-        int col;
-        public Triplet(int time, int row, int col) {
-            this.time = time;
-            this.row = row;
-            this.col = col;
-        }
-    }
     public int minimumTime(int[][] grid) {
-        // TC : O((M * N) log (M * N)) // SC : O(M*N)
-        if(grid[0][1] > 1 && grid[1][0] > 1) return -1;
+        // TC : O(M*N) log (M*N) // SC : O(M*N)
+        if(grid[0][1] > 1 && grid[1][0] > 1) {
+            return -1;
+        }
         int m = grid.length;
         int n = grid[0].length;
-        PriorityQueue<Triplet> pq = new PriorityQueue<>((x,y) -> x.time-y.time);
         int[][] vis = new int[m][n];
-        int[] delrow = {-1,0,1,0};
-        int[] delcol = {0,1,0,-1};
-        pq.offer(new Triplet(0, 0, 0));
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[2]-b[2]);
+        pq.offer(new int[]{0,0,0});
+        int[] delrow = {0,1,0,-1};
+        int[] delcol = {1,0,-1,0};
         while(!pq.isEmpty()) {
-            int time = pq.peek().time;
-            int row = pq.peek().row;
-            int col = pq.peek().col;
+            int row = pq.peek()[0];
+            int col = pq.peek()[1];
+            int time = pq.peek()[2];
             pq.poll();
-            if(row == m-1 && col == n-1) return time;
-            if(vis[row][col] == 1) continue;
+            if(row == m-1 && col == n-1) {
+                return time;
+            }
+            if(vis[row][col] == 1) {
+                continue;
+            }
             vis[row][col] = 1;
             for(int i = 0; i < 4; i++) {
-                int nrow = row + delrow[i];
-                int ncol = col + delcol[i];
-                if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && vis[nrow][ncol] == 0) {
+                int nrow = row+delrow[i];
+                int ncol = col+delcol[i];
+                if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n) {
                     if(grid[nrow][ncol] <= time+1) {
-                        pq.offer(new Triplet(time+1, nrow, ncol));
+                        pq.offer(new int[]{nrow, ncol, time+1});
                     }
                     else {
-                        int diff = grid[nrow][ncol] - time;
-                        if(diff % 2 == 1) {
-                            pq.offer(new Triplet(grid[nrow][ncol], nrow, ncol));
+                        int diff = grid[nrow][ncol]-time;
+                        if(diff%2 == 1) {
+                            pq.offer(new int[]{nrow, ncol, grid[nrow][ncol]});
                         }
                         else {
-                            pq.offer(new Triplet(grid[nrow][ncol]+1, nrow, ncol));
+                            pq.offer(new int[]{nrow, ncol, grid[nrow][ncol]+1});
                         }
-                    }
+                    } 
                 }
             }
         }
