7a1d82153275451c948c97a31f09bcae8e3b77b3
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-12-01 01:41:14 +0530

    Time: 71 ms (79.55%), Space: 127.3 MB (86.41%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,26 +1,31 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        // TC : O(V+2E) // SC : O(V+2E) + O(N) + O(N)
-        if(n == 1) return true; 
+        // TC : O(V+2E) // SC : O(V+2E)
+        if(n == 1) {
+            return true;
+        }
         List<List<Integer>> adj = new ArrayList<>();
         for(int i = 0; i < n; i++) {
             adj.add(new ArrayList<>());
         }
-        for(int i = 0; i < edges.length; i++) {
+        int m = edges.length;
+        for(int i = 0; i < m; i++) {
             adj.get(edges[i][0]).add(edges[i][1]);
             adj.get(edges[i][1]).add(edges[i][0]);
         }
         Queue<Integer> q = new LinkedList<>();
-        q.add(source);
+        q.offer(source);
         int[] vis = new int[n];
-        vis[source] = 1; 
+        vis[source] = 1;
         while(!q.isEmpty()) {
             int node = q.poll();
             for(int it : adj.get(node)) {
-                if(it == destination) return true; 
+                if(it == destination) {
+                    return true;
+                }
                 if(vis[it] == 0) {
                     vis[it] = 1;
-                    q.add(it);
+                    q.offer(it);
                 }
             }
         }
