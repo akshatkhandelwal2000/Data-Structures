68d0ea6e6c8c0ce9725c2afed8184caa8476f22a
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-12-12 00:10:34 +0530

    Time: 37 ms (82.97%), Space: 60.2 MB (50%) - LeetHub

--- 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.java
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.java
@@ -1,10 +1,11 @@
 class Solution {
     public int maximumBeauty(int[] nums, int k) {
         // TC : O(NlogN) // SC : O(1)
+        int n = nums.length;
         Arrays.sort(nums);
         int j = 0, i = 0;
-        for(j = 0; j < nums.length; j++) {
-            if(nums[j] - nums[i]> k*2) {
+        for(j = 0; j < n; j++) {
+            if(nums[j]-nums[i] > k*2) {
                 i++;
             }
         }
