46651daff965946a534ca04ad20fdccf4d92c577
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-12-17 00:29:04 +0530

    Time: 4 ms (16.55%), Space: 45 MB (34.76%) - LeetHub

--- 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.java
+++ 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.java
@@ -2,21 +2,23 @@ class Solution {
     public int[] getFinalState(int[] nums, int k, int multiplier) {
         // TC : O(NlogN) // SC : O(N)
         int n = nums.length;
+        int[] ans = new int[n];
         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> {
-            if(a[0] == b[0]) {
+            if(a[0]==b[0]) {
                 return a[1]-b[1];
             }
-            else return a[0]-b[0];
+            else {
+                return a[0]-b[0];
+            }
         });
         for(int i = 0; i < n; i++) {
             pq.offer(new int[]{nums[i], i});
         }
-        int[] ans = new int[n];
-        while(k > 0) {
-            int[] i = pq.poll();
-            i[0] = i[0]*multiplier;
-            pq.offer(i);
-            k--;
+        while(k-- > 0) {
+            int a = pq.peek()[0];
+            int b = pq.peek()[1];
+            pq.poll();
+            pq.offer(new int[]{a*multiplier, b});
         }
         while(!pq.isEmpty()) {
             ans[pq.peek()[1]] = pq.peek()[0];
