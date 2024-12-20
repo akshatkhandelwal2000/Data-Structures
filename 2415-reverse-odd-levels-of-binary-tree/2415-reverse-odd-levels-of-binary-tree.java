88bbb28a095adaa16cb5961e31eb15b8d2a43c4d
Akshat khandelwal <akshatkhandelwal48@gmail.com>
2024-12-21 02:35:16 +0530

    Time: 12 ms (18.25%), Space: 48.4 MB (61.82%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.java
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.java
@@ -15,43 +15,46 @@
  */
 class Solution {
     public TreeNode reverseOddLevels(TreeNode root) {
-        // TC : O(N) // SC : O(H)
-        dfs(root.left, root.right, 1);
-        return root;
-    }
-    public void dfs(TreeNode root1, TreeNode root2, int level) {
-        if(root1 == null || root2 == null) return;
-        if(level % 2 != 0) {
-            int temp = root1.val;
-            root1.val = root2.val;
-            root2.val = temp;
-        }
-        dfs(root1.left, root2.right, level + 1);
-        dfs(root1.right, root2.left, level + 1);
         // TC : O(N) // SC : O(N)
-        // Queue<TreeNode> q = new LinkedList<>();
-        // q.offer(root);
-        // int level = 0;
-        // while(!q.isEmpty()) {
-        //     int size = q.size();
-        //     int in = 0;
-        //     for(int i = 0; i < size; i++) {
-        //         TreeNode curr = q.poll();
-        //         if(curr.left != null) q.offer(curr.left);
-        //         if(curr.right != null) q.offer(curr.right);
-        //     }
-        //     level++;
-        //     if(level % 2 != 0) {
-        //         int[] nums = new int[q.size()];
-        //         for(TreeNode p : q) {
-        //             nums[in++] = p.val;
-        //         }
-        //         int sz = q.size()-1;
-        //         for(TreeNode p : q) {
-        //             p.val = nums[sz--];
-        //         }
-        //     }
-        // }
-        // return root;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        int in = 0;
+        while(!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> list = new ArrayList<>();
+            if(in%2 == 0) {
+                for(int i = 0; i < size; i++) {
+                    TreeNode curr = q.poll();
+                    if(curr.left != null) {
+                        if(in%2 == 0) {
+                            list.add(curr.left.val);
+                    }
+                    q.offer(curr.left);
+                    }
+                    if(curr.right != null) {
+                        if(in%2 == 0) {
+                            list.add(curr.right.val);
+                    }
+                    q.offer(curr.right);
+                    }
+                }
+            }
+            in++;
+            if(in%2 != 0) {
+                size = q.size();
+                Collections.reverse(list);
+                for(int i = 0; i < size; i++) {
+                    TreeNode curr = q.poll();
+                    curr.val = list.get(i);
+                    if(curr.left != null) {
+                        q.offer(curr.left);
+                    }
+                    if(curr.right != null) {
+                        q.offer(curr.right);
+                    }
+                }
+            }
+        }
+        return root;
     }
 }
